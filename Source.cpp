71d9403d81ab8346da05e9cbfa2e34a83bc6cf36
ImDatAsian <smartcow831@gmail.com>
2020-07-04 20:26:25 -0500

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,36 +1,57 @@
 #include "Date.h";
-#include <stack>
+#include "queue.h"
 #include <iostream>
+
+void book_Circulation(queue<string> booksArchived, queue<string> booksCirculation, Date dateOne, Date dateTwo, Date dateThree) {
+	string temp;
+
+	while (booksArchived.size() != 0) {
+		string temp = booksArchived.front();
+		booksArchived.RemoveBook();
+		booksCirculation.addBooks(temp);
+		cout << temp << endl;
+		cout << booksArchived.size() << endl;
+		if (booksArchived.size() == 0) {
+			break;
+		}
+	}
+}
+
 
 
 int main() {
 	/*List of books and different list for where books can go*/
-	stack<string> books_In_Circulation;
-	stack<string> books_Archived;
-	stack<string> books;
-	/*Sending books into the books stack*/
-	books.push("How to Code with Javascript");
-	books.push("How to Code with Python");
-	books.push("How to Code with Java");
-	books.push("How to Code with C++");
+	queue<string> books_Archived;
+	queue<string> books_in_Circulation;
+
+	books_Archived.addBooks("How to Code with Python");
+	books_Archived.addBooks("How to Code with Java");
+	books_Archived.addBooks("How to Code with C++");
+	books_Archived.addBooks("The Second Coming of Gluttony");
+	books_Archived.addBooks("Overgeard");
 
 	/*Setting the date in YYYY-MM-DD format*/
-	Date date_1(2020, 2, 10); 
-	Date date_2(2020, 3, 21);
-	Date date_3();
+	Date date_1(2020, 2, 10);
+	Date date_2(2020, 2, 11);
+	Date date_3(2020, 2, 15);
 	Date date_4();
-	Date date_5();
+
+	book_Circulation(books_Archived, books_in_Circulation, date_1, date_2, date_3);
+
+
+
+
+
+
+
 
 	while (date_1 != date_2) //checks whether the first date is equal to the second date
 	{
 		date_1.add_days(1); // adds a day to the first date
-		string firstDate = date_1.toString(); // converts the date into a string
-		cout << firstDate << endl; // outputs the first date to the console
-
 		if (date_1 == date_2) { // checks if the first date is equal to the second date
 			break; // breaks the loop
 		}
 	}
 
-	
+	return 0;
 }
